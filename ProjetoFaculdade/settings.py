352ec0940dc970114ad48e2c3abef6551488bca7
Guilhermekek <kenny-300@outlook.com>
2024-12-03 17:16:08 -0300

    Sql realizado a criaçao do banco de dados e do super user

--- ProjetoFaculdade/settings.py
+++ ProjetoFaculdade/settings.py
@@ -77,11 +77,17 @@ WSGI_APPLICATION = 'ProjetoFaculdade.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'academia_db',
+        'USER': 'guilherme',
+        'PASSWORD': 'cassia',
+        'HOST': 'localhost',  # Or your database host
+        'PORT': '3306',       # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
