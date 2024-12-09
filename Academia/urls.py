a1ec32453034ed2a99f31f8834005c5ff675c5c9
Guilhermekek <kenny-300@outlook.com>
2024-12-08 23:31:51 -0300

    Remover Criado o botao de remover treino e exercicio

--- Academia/urls.py
+++ Academia/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('adicionar_treino/', views.adicionar_treino, name='adicionar_treino'),
     path('treino/<int:treino_id>/adicionar_exercicio/', views.adicionar_exercicio, name='adicionar_exercicio'),
+    path('remover_treino/<int:treino_id>/', views.remover_treino, name='remover_treino'),
+    path('remover_exercicio/<int:exercicio_id>/', views.remover_exercicio, name='remover_exercicio'),
 
 ]
